56b4fc89f569f48b0746e3ee24e54c8491643358
AmandaAnguiano <amanda.anguiano@ucas-edu.net>
2024-09-11 18:55:39 +0000

    added variables

---
+++ practice.c
@@ -0,0 +1,13 @@
+int main(void) {
+char name[] = "amanda";
+int upToTen = 5; 
+int number = 200; 
+char breakfast[] = "cereal";
+char favColor[] = "purple";
+char school[] = "UCAS";  
+int year = 2024;
+char eyeColor[] = "brown";
+int age = 14;
+char favSubject[] = "geography";
+printf(%s, %d, %d, %s, %s, %s, %d, %s, %d, %s, name, upToTen, number, breakfast, favColor, school, year, eyeColor, age, favSubject)
+}
---
+++ practice.py
@@ -0,0 +1,11 @@
+name= ("amanda")
+upToTen= 5
+numbers= 200
+breakfast= ("cereal") 
+favColor= ("purple")
+school= ("UCAS")
+year= 2024
+eyeColor= ("brown")
+age= 14
+favSubject= ("geography") 
+print(name, upToTen, numbers, breakfast, favColor, school, year, eyeColor, age, favSubject)
