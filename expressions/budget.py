841c1cb3615640e8f4c0b2a5795851fa73c5e06b
AmandaAnguiano <amanda.anguiano@ucas-edu.net>
2024-09-18 13:03:06 +0000

    i just did the assignment

--- expressions/budget.py
+++ expressions/budget.py
@@ -1,3 +1,21 @@
 print("This is a finacial calculator")
 income = input("how much do you make a month?:\n")
 rent = input("how much is your rent?:\n")
+utilitties=input("How much is your utilitties?:\n")
+groceries=input("How much do your goceries cost?:\n")
+transp=input("What does your transportation cost?:\n")
+print("You make", income)
+savings = float(income)*float(0.2)
+expenses = (float(rent) + float(utilitties) + float(groceries) + float(transp))
+spending = float(income)-float(expenses)-float(savings)
+print("your expenses are", expenses)
+print("your savings is", savings)
+print("your spending money is", spending)
+prent=(float(rent)/float(income))*100 
+putilitties=(float(utilitties)/float(income))*100
+pgroceries=(float(groceries)/float(income))*100
+ptransp=(float(transp)/float(income))*100
+print("your rent takes up this percent of your income;", prent)
+print("your utilitties take up this percent of your income;", putilitties)
+print("your groceries take up this percent of your income;", pgroceries)
+print("you transportation takes up this percent of your income;", ptransp)
