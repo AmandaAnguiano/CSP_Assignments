20b26f7dd24a82ab52d9a221d26b1f4338194a9e
AmandaAnguiano <amanda.anguiano@ucas-edu.net>
2024-09-19 19:44:04 +0000

    finished the assignment

--- expressions/strings/sillystrings.c
+++ expressions/strings/sillystrings.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(void){
 char animal[20];
 char place[20];
 char verb[20];
-printf("Name an animal: \n");
-scanf("%s",animal);
-printf("Name a place: \n");
-scanf("%s",place);
-printf("Name a verb: \n");
-scanf("%s",verb);
-    printf("The %s walked to the %s and did %s with his friend the angry turtle.\n", animal, place, verb);
+char sentence[400]= "The ";
+printf("Name an animal: ");
+scanf("%s", animal);
+printf("Name a place: ");
+scanf("%s", place);
+printf("Name a past tense verb: ");
+scanf("%s", verb);
+strcat(sentence, animal);
+strcat(sentence, " went to the ");
+strcat(sentence, place);
+strcat(sentence, " and ");
+strcat(sentence, verb);
+strcat(sentence, " with his friend the angry turtle. \n");
+    printf("%s", sentence);
     return 0;
 }
