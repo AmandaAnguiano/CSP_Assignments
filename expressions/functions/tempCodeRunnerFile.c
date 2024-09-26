bdb45cfe93bc68c9a15aaa42a718bafa13e9f178
AmandaAnguiano <amanda.anguiano@ucas-edu.net>
2024-09-26 19:16:32 +0000

    did the stuff i needed to do

---
+++ expressions/functions/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+void name(char name[]){
+  printf("hello,%s! \n", name); 
+}
+int main() {
+ name("Maddie");
+ name("Adele");
+ name("Kayla");
+ name("Sofia");
+ name("Lola"); 
+    return 0;
+}
